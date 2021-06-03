413b387fa53df1163ae953f373f7f0ba10445d57
Oleksii Ovcharenko <938082+ovcharenko@users.noreply.github.com>
2021-06-03 12:45:37 +0000

    [ADD] Optional tags

--- variables.tf
+++ variables.tf
@@ -5,3 +5,8 @@ variable "name" {
   description = "The name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
 }
 
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Resource tags"
+}
